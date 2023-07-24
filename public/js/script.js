0556864e8cde08e068f16b163ace3a2130767f30
Alex Gay <alexgay27753@gmail.com>
2023-07-24 17:56:13 +0800

    scrollreveal commit

--- public/js/script.js
+++ public/js/script.js
@@ -37,7 +37,6 @@ var swiper = new Swiper(".reviews-slider", {
    },
 });
 
-
 /* ==========scroll reveal========= */
 ScrollReveal({ 
    // reset: true,
@@ -47,43 +46,8 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.swiper, .heading-title', { origin: 'top' });
-ScrollReveal().reveal('.reviews, .home-offer, .home-packages, .home-about, .box-container, .booking', { origin: 'bottom' });
-ScrollReveal().reveal('.about, .partner, .services, .heading', { origin: 'left' });
+ScrollReveal().reveal('.about, .reviews, .home-offer, .home-packages, .home-about, .box-container, .booking', { origin: 'bottom' });
+ScrollReveal().reveal('.partner, .services, .heading', { origin: 'left' });
 ScrollReveal().reveal('.tables, .about-content', { origin: 'right' });
 
-
-document.addEventListener("DOMContentLoaded", function () {
-   const form = document.getElementById("bookingForm");
-   form.addEventListener("submit", handleSubmit);
- });
- 
- function handleSubmit(event) {
-   event.preventDefault();
- 
-   const name = document.getElementById("name").value;
-   const email = document.getElementById("email").value;
-   const phone = document.getElementById("phone").value;
-   const address = document.getElementById("address").value;
-   const placeToVisit = document.getElementById("placeToVisit").value;
-   const numberOfGuests = document.getElementById("numberOfGuests").value;
-   const arrivalDate = document.getElementById("arrivalDate").value;
-   const departureDate = document.getElementById("departureDate").value;
- 
-   // Save the form data to local storage
-   saveFormDataToLocalStorage(name, email, phone, address, placeToVisit, numberOfGuests, arrivalDate, departureDate);
- 
-   // Show success alert
-   showSuccessAlert();
-   console.log(name)
- }
- 
- function saveFormDataToLocalStorage(name, email, phone, address, placeToVisit, numberOfGuests, arrivalDate, departureDate) {
-   // You can store the data as an object or any other desired format
-   const formData = { name, email, phone, address, placeToVisit, numberOfGuests, arrivalDate, departureDate };
-   localStorage.setItem("formData", JSON.stringify(formData));
- }
- 
- function showSuccessAlert() {
-   alert("Form data submitted successfully!");
- }
  
