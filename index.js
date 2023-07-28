cb4946890e69558c9b67510fb0a044263d7b1933
Alex Gay <alexgay27753@gmail.com>
2023-07-28 16:32:11 +0800

    register&faviconcommit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
+var favicon = require('serve-favicon')
+var path = require('path')
+var cookieParser = require('cookie-parser')
 
 const app = express()
 const port1 = 3000; // First port (for frontend)
@@ -8,8 +11,11 @@ const port1 = 3000; // First port (for frontend)
 // Use bodyParser middleware to parse JSON data from requests
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon-32x32.png')));
 
 // Home Route
 app.get('/', (req, res) => {
@@ -30,7 +36,7 @@ app.get('/book', (req, res) => {
 })
 
 // Connect to MongoDB (replace 'YOUR_MONGODB_URI' with your actual MongoDB connection URI)
-mongoose.connect('mongodb+srv://alexgay929:1DsuExsvo8aX9vCX@cluster0.fsy3wif.mongodb.net/formdatasDB', {
+mongoose.connect('mongodb+srv://alexgay929:1DsuExsvo8aX9vCX@cluster0.fsy3wif.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -40,38 +46,90 @@ var db = mongoose.connection;
 db.on('error',()=>console.log("Error in Connecting to Database"));
 db.once('open',()=>console.log("Connected to Database"))
 
-app.post("/bookings", (req, res)=>{
+// Create Mongoose schema for Form datas
+const Form1Schema = new mongoose.Schema({
+    name : String,
+    email : String,
+    phone : String,
+    address : String,
+    location : String,
+    guests : Number,
+    arrival : String,
+    departure : String,
+    // Add other fields as needed for Form Datas
+  });
+  
+  // Create Mongoose model for Form datas
+  const Form1Model = mongoose.model('FormDatas', Form1Schema);
+  
+  // Create Mongoose schema for Sign Up Formdata
+  const Form2Schema = new mongoose.Schema({
+    fullname : String,
+    signupEmail : String,
+    signupPassword : String,
+    // Add other fields as needed for Form2
+  });
+  
+  // Create Mongoose model for Sign Up Formdata
+  const Form2Model = mongoose.model('SignupformDatas', Form2Schema);
+  
+  // Route to handle Formdatas
+  app.post('/bookings', (req, res) => {
     var name = req.body.name;
-    var email = req.body.email;
-    var phone = req.body.phone;
-    var address = req.body.address;
-    var location = req.body.location;
-    var guests = req.body.guests;
-    var arrival = req.body.arrival;
-    var departure = req.body.departure;
-
-    var data = {
-        "name" : name,
-        "email" : email,
-        "phone" : phone,
-        "address" : address,
-        "location" : location,
-        "guests" : guests,
-        "arrival" : arrival,
-        "departure" : departure
-    }
-
-    db.collection('formdatas').insertOne(data,(err,collection)=>{
-        if(err){
-            throw err;
-        }
-        console.log("Record Inserted Successfully!");
-    });
-
+         var email = req.body.email;
+         var phone = req.body.phone;
+         var address = req.body.address;
+         var location = req.body.location;
+         var guests = req.body.guests;
+         var arrival = req.body.arrival;
+         var departure = req.body.departure;
+    
+         var data = {
+             "name" : name,
+             "email" : email,
+             "phone" : phone,
+            "address" : address,
+             "location" : location,
+             "guests" : guests,
+             "arrival" : arrival,
+             "departure" : departure
+         }
+    
+        db.collection('formdatas').insertOne(data,(err,collection)=>{
+             if(err){
+                 throw err;
+             }
+             console.log("Record Inserted Successfully!");
+        });
+    
     return res.redirect('/success.html')
-})
+    
+  });
 
-app.get("/", (req, res)=> {
+  
+  // Route to handle Form2 data
+  app.post('/sign_up', (req, res) => {
+    // Create a new document for Form2 data
+         var signupFullname = req.body.signupFullname;
+         var signupEmail = req.body.signupEmail;
+         var signupPassword = req.body.signupPassword;
+    
+         var data = {
+             "name" : signupFullname,
+             "email" :  signupEmail,
+             "password" : signupPassword,
+    }
+    db.collection('signupformdatas').insertOne(data,(err,collection)=>{
+                 if(err){
+                     throw err;
+                 }
+                 console.log("Record Inserted Successfully!");
+             });
+        
+             return res.redirect('/signupsuccess.html')
+        });
+
+  app.get("/", (req, res)=> {
     res.set({
         "Allow-access-Allow-origin": '*'
     })
